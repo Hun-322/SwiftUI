f3d05b195d1c3ce77eef8dd5b110e2ff81c9d4b7
Hun <ksh26271@naver.com>
2023-11-29 21:05:12 +0900

    [add] project init

---
+++ SwiftuiCrypto/SwiftuiCrypto/SwiftuiCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiCryptoApp.swift
+//  SwiftuiCrypto
+//
+//  Created by KSH on 11/29/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
