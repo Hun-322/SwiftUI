a45f3fcee336d84a60ab0dc6e851f8b9e070e0d3
Hun <ksh26271@naver.com>
2023-11-29 21:39:29 +0900

    [add] Asset Color, Image 추가

---
+++ SwiftuiCrypto/SwiftuiCrypto/Extensions/Color.swift
@@ -0,0 +1,23 @@
+//
+//  Color.swift
+//  SwiftuiCrypto
+//
+//  Created by KSH on 11/29/23.
+//
+
+import SwiftUI
+
+extension Color {
+    
+    static let theme = ColorTheme()
+    
+}
+
+struct ColorTheme {
+    
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("redColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
