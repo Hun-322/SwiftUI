4fcff5a7b732589ac3e8d73568c2b6161b84c3ce
Hun <ksh26271@naver.com>
2023-12-02 01:01:28 +0900

    [feat(CoinRowView)] CoinRowView UI 구현

---
+++ SwiftuiCrypto/SwiftuiCrypto/Core/Home/Views/CoinRowView.swift
@@ -0,0 +1,81 @@
+//
+//  CoinRowView.swift
+//  SwiftuiCrypto
+//
+//  Created by KSH on 12/1/23.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    
+    let coin: CoinModel
+    let showHoldingsColumn: Bool
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            leftColumn
+            Spacer()
+            if showHoldingsColumn {
+                centerColumn
+            }
+            rightColumn
+
+        }
+        .font(.subheadline)
+    }
+}
+
+struct CoinRowView_Preview: PreviewProvider {
+    static var previews: some View {
+        Group {
+            CoinRowView(coin: dev.coin, showHoldingsColumn: true)
+                .previewLayout(.sizeThatFits)
+            
+            CoinRowView(coin: dev.coin, showHoldingsColumn: true)
+                .previewLayout(.sizeThatFits)
+                .preferredColorScheme(.dark)
+        }
+    }
+}
+
+
+extension CoinRowView {
+    
+    private var leftColumn: some View {
+        HStack(spacing: 0) {
+            Text("\(coin.rank)")
+                .font(.caption)
+                .foregroundColor(.theme.secondaryText)
+                .frame(minWidth: 30)
+            Circle()
+                .frame(width: 30, height: 30)
+            Text(coin.symbol.uppercased())
+                .font(.headline)
+                .padding(.leading, 6)
+                .foregroundColor(.theme.accent)
+        }
+    }
+    
+    private var centerColumn: some View {
+        VStack(alignment: .trailing) {
+            Text(coin.currentHoldingsValue.asCurrnecy())
+                .bold()
+            Text((coin.currnetHoldings ?? 0).asNumberString())
+        }
+        .foregroundColor(.theme.accent)
+    }
+    
+    private var rightColumn: some View {
+        VStack(alignment: .trailing) {
+            Text("\(coin.currentPrice.asCurrnecy())")
+                .bold()
+                .foregroundColor(.theme.accent)
+            Text(coin.priceChangePercentage24H?.asPercentString() ?? "")
+                .foregroundColor(
+                    (coin.priceChangePercentage24H ?? 0) >= 0 ? .theme.green : .theme.red
+                )
+        }
+        .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
+    }
+}
---
+++ SwiftuiCrypto/SwiftuiCrypto/Extensions/Double.swift
@@ -0,0 +1,8 @@
+//
+//  Double.swift
+//  SwiftuiCrypto
+//
+//  Created by KSH on 12/1/23.
+//
+
+import Foundation
