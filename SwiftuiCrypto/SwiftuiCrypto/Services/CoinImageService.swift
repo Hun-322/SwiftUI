9cb2839f4383fc0ab2e4cfc2a7a746230c9b3657
Hun <ksh26271@naver.com>
2023-12-06 23:13:11 +0900

    [feat(LocalFileManager)] FileManager를 통해 이미지 저장 로직 구현

--- SwiftuiCrypto/SwiftuiCrypto/Services/CoinImageService.swift
+++ SwiftuiCrypto/SwiftuiCrypto/Services/CoinImageService.swift
@@ -15,22 +15,38 @@ class CoinImageService {
     
     private var imageSubscription: AnyCancellable?
     private let coin: CoinModel
+    private let fileManager = LocalFileManager.instance
+    private let folderName = "coin_image"
+    private let imageName: String
     
     init(coin: CoinModel) {
         self.coin = coin
+        self.imageName = coin.id
         getCoinImage()
     }
     
     private func getCoinImage() {
+        if let savedImage = fileManager.getImage(imageName: imageName, folderName: folderName) {
+            image = savedImage
+            print("파일 관리자에서 이미지를 검색했습니다.")
+        } else {
+            print("이미지 다운로드 중입니다.")
+            downloadCoinImage()
+        }
+    }
+    
+    private func downloadCoinImage() {
         guard let url = URL(string: coin.image) else { return }
         
         imageSubscription = NetworkingManager.download(url: url)
             .tryMap({ data -> UIImage? in
                 return UIImage(data: data)
             })
-            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: {[weak self] (returnImage) in
-                self?.image = returnImage
-                self?.imageSubscription?.cancel()
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: {[weak self] (returndImage) in
+                guard let self = self, let downloadImage = returndImage else { return }
+                self.image = returndImage
+                self.imageSubscription?.cancel()
+                self.fileManager.saveImage(image: downloadImage, imageName: imageName, folderName: folderName)
             })
     }
 }
---
+++ SwiftuiCrypto/SwiftuiCrypto/Services/LocalFileManager.swift
@@ -0,0 +1,69 @@
+//
+//  LocalFileManager.swift
+//  SwiftuiCrypto
+//
+//  Created by KSH on 12/6/23.
+//
+
+import Foundation
+import SwiftUI
+
+class LocalFileManager {
+    
+    static let instance = LocalFileManager()
+    private init() { }
+    
+    func saveImage(image: UIImage, imageName: String, folderName: String) {
+        
+        // 폴더 생성
+        createFolderIfNeeded(folderName: folderName)
+        
+        // 이미지의 주소 가져오기
+        guard
+            let data = image.pngData(),
+            let url = getURLForImage(imageName: imageName, folderName: folderName)
+        else { return }
+        
+        // 이미지 주소 저장
+        do {
+            try data.write(to: url)
+        } catch let error {
+            print("이미지를 저장중에 오류가 생겼습니다. ImageName: \(imageName). \(error)")
+        }
+    }
+    
+    func getImage(imageName: String, folderName: String) -> UIImage? {
+        
+        guard
+            let url = getURLForImage(imageName: imageName, folderName: folderName),
+            FileManager.default.fileExists(atPath: url.path)
+        else { return nil }
+        
+        return UIImage(contentsOfFile: url.path)
+    }
+    
+    private func createFolderIfNeeded(folderName: String) {
+        guard let url = getURLForFolder(folderName: folderName) else { return }
+        
+        if !FileManager.default.fileExists(atPath: url.path) {
+            do {
+                try FileManager.default.createDirectory(at: url, withIntermediateDirectories: true)
+            } catch let error {
+                print("directory 생성하는중에 오류가 생겼습니다. FolderName: \(folderName). \(error)")
+            }
+        }
+    }
+    
+    private func getURLForFolder(folderName: String) -> URL? {
+        
+        guard let url = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first
+        else { return nil }
+        
+        return url.appendingPathComponent(folderName)
+    }
+    
+    private func getURLForImage(imageName: String, folderName: String) -> URL? {
+        guard let folderURL = getURLForFolder(folderName: folderName) else { return nil }
+        return folderURL.appendingPathComponent(imageName + ".png")
+    }
+}
