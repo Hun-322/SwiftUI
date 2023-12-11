ddc6e879539be14132051e60a9fc3c9ed57ec155
Hun <ksh26271@naver.com>
2023-12-11 23:56:24 +0900

    [feat(MarketData)] API통신으로 MarketData 다운로드 및 UI표시 구현

---
+++ SwiftuiCrypto/SwiftuiCrypto/Models/MarketDataModel.swift
@@ -0,0 +1,8 @@
+//
+//  MarketDataModel.swift
+//  SwiftuiCrypto
+//
+//  Created by KSH on 12/11/23.
+//
+
+import Foundation
---
+++ SwiftuiCrypto/SwiftuiCrypto/Services/MarketDataService.swift
@@ -0,0 +1,8 @@
+//
+//  MarketDataService.swift
+//  SwiftuiCrypto
+//
+//  Created by KSH on 12/11/23.
+//
+
+import Foundation
