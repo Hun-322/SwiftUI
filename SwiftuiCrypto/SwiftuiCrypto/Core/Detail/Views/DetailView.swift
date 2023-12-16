9ab100f464045b0ecd55c01ed66da2a6f1b6387e
Hun <ksh26271@naver.com>
2023-12-16 21:17:03 +0900

    [feat(DetailView)] DetailView 구현

---
+++ SwiftuiCrypto/SwiftuiCrypto/Core/Detail/Views/DetailView.swift
@@ -0,0 +1,41 @@
+//
+//  DetailView.swift
+//  SwiftuiCrypto
+//
+//  Created by KSH on 12/16/23.
+//
+
+import SwiftUI
+
+struct DetailLoadingView: View {
+
+    @Binding var coin: CoinModel?
+    
+    var body: some View {
+        ZStack {
+            if let coin = coin {
+                DetailView(coin: coin)
+            }
+        }
+    }
+}
+
+struct DetailView: View {
+    
+    let coin: CoinModel
+    
+    init(coin: CoinModel) {
+        self.coin = coin
+        print("Initializing Detail View for \(coin.name)")
+    }
+    
+    var body: some View {
+        Text(coin.name)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(coin: dev.coin)
+    }
+}
--- SwiftuiCrypto/SwiftuiCrypto/Core/Home/Views/HomeView.swift
+++ SwiftuiCrypto/SwiftuiCrypto/Core/Home/Views/HomeView.swift
@@ -13,6 +13,9 @@ struct HomeView: View {
     @State private var showPortfolio: Bool = false // animate right
     @State private var showPortfolioView: Bool = false // new sheet
     
+    @State private var selectedCoin: CoinModel? = nil
+    @State private var showDetailView: Bool = false
+    
     var body: some View {
         ZStack {
             // background layer
@@ -43,6 +46,13 @@ struct HomeView: View {
                 Spacer(minLength: 0)
             }
         }
+        .background(
+            NavigationLink(
+                destination: DetailLoadingView(coin: $selectedCoin),
+                isActive: $showDetailView,
+                label: { EmptyView() }
+            )
+        )
     }
 }
 
@@ -94,6 +104,9 @@ extension HomeView {
             ForEach(vm.allCoins) { coin in
                 CoinRowView(coin: coin, showHoldingsColumn: false)
                     .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
+                    .onTapGesture {
+                        segue(coin: coin)
+                    }
             }
         }
         .listStyle(PlainListStyle())
@@ -104,9 +117,17 @@ extension HomeView {
             ForEach(vm.portfolioCoins) { coin in
                 CoinRowView(coin: coin, showHoldingsColumn: true)
                     .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
+                    .onTapGesture {
+                        segue(coin: coin)
+                    }
             }
         }
         .listStyle(PlainListStyle())
+    }
+    
+    private func segue(coin: CoinModel) {
+        selectedCoin = coin
+        showDetailView.toggle()
     }
     
     private var columnTitles: some View {
@@ -148,7 +169,7 @@ extension HomeView {
                     vm.sortOption = vm.sortOption == .price ? .priceReversed : .price
                 }
             }
-
+            
             
             Button(action: {
                 withAnimation(.linear(duration: 2.0)) {
