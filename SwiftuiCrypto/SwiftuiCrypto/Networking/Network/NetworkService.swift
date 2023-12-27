165042d37d72e5146415f0f33609adf9a2059b7a
Hun <ksh26271@naver.com>
2023-12-27 17:35:18 +0900

    [refactor(NetworkService)] URLSession 의존성 주입

--- SwiftuiCrypto/SwiftuiCrypto/Networking/Network/NetworkService.swift
+++ SwiftuiCrypto/SwiftuiCrypto/Networking/Network/NetworkService.swift
@@ -8,6 +8,17 @@
 import Foundation
 import Combine
 
+// URLSession을 의존성 주입을 하는 이유
+// 네트워크 통신이 되지 않는 환경에서도 나머지 데이터를 처리하는 로직이 잘 작동하는지 테스트할 수 있는 코드를 만들 수 있음.
+// 네트워크 통신이 안되더라도 (data, response, error의 처리에 대한)나머지 로직이 잘 굴러가는지 확인해보기위해서(Test할때 MockURLSession을 넣어서 실험 가능)
+// dataTask(dataTaskPublisher) 작업을 가로채는 것!! 을 강조 한 부분 기본 URLSession 동작을 하는 class를 만드는 dataTask 메소드를 통해 session의 dataTask 메소드를 호출해 사용한다.
+// URLSession의 네트워크 동작이 어떻게 동작하던 dataTask를 선언한 메소드의 데이터 흐름을 제어한다면 언제든지 네트워크와 무관한 모델 테스트가 가능해져서 의존성 주입을 한다.
+protocol URLSessionProtocol {
+    func dataTaskPublisher(for request: URLRequest) -> URLSession.DataTaskPublisher
+}
+
+extension URLSession: URLSessionProtocol {}
+
 protocol NetworkServable {
     func request<API>(_ api: API) -> AnyPublisher<API.Response, NetworkError> where API: ServableAPI
     func request(url: URL) -> AnyPublisher<Data, NetworkError>
@@ -15,15 +26,16 @@ protocol NetworkServable {
 }
 
 class NetworkService: NetworkServable {
-        
-    init() {}
+    let session: URLSessionProtocol
+    
+    init(session: URLSessionProtocol = URLSession.shared) {
+        self.session = session
+    }
     
     func request<API>(
         _ api: API
     ) -> AnyPublisher<API.Response, NetworkError>
     where API: ServableAPI {
-        let session = URLSession.shared
-        
         return session.dataTaskPublisher(for: api.urlRequest)
             .tryMap { data, response in
                 try self.validate(response)
@@ -35,9 +47,8 @@ class NetworkService: NetworkServable {
     }
     
     func request(url: URL) -> AnyPublisher<Data, NetworkError> {
-        let session = URLSession.shared
-        
-        return session.dataTaskPublisher(for: url)
+        let urlRequest = URLRequest(url: url)
+        return session.dataTaskPublisher(for: urlRequest)
             .tryMap { data, response in
                 try self.validate(response)
                 return data
@@ -53,7 +64,7 @@ class NetworkService: NetworkServable {
         case .finished:
             break
         case .failure(let error):
-            print(error.localizedDescription)
+            print("네트워크 request 실패 오류입니다! \(error.localizedDescription)")
         }
     }
 }
