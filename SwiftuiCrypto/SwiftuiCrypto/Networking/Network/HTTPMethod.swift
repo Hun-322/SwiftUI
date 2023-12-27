ec632e705d7a72cc490c0b203282fa8d00d1e119
Hun <ksh26271@naver.com>
2023-12-27 17:24:12 +0900

    [refactor(HTTPMethod)] HTTPMethod enum 추가

---
+++ SwiftuiCrypto/SwiftuiCrypto/Networking/Network/HTTPMethod.swift
@@ -0,0 +1,14 @@
+//
+//  HTTPMethod.swift
+//  SwiftuiCrypto
+//
+//  Created by KSH on 12/27/23.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get     = "GET"
+    case post    = "POST"
+    case delete  = "DELETE"
+}
